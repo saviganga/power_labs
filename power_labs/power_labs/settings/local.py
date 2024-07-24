b7d5c6338b4a640741be4498f64982979dbbf89f
saviganga <akatugbasaviour@gmail.com>
2024-07-24 13:47:02 +0100

    feat: local environment db

--- power_labs/power_labs/settings/local.py
+++ power_labs/power_labs/settings/local.py
@@ -5,7 +5,16 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
-    'default': {
+
+    "default": {        
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get('POSTGRES_DB'),
+        "USER": os.environ.get('POSTGRES_USER'),
+        "PASSWORD": os.environ.get('POSTGRES_PASSWORD'),
+        "HOST": 'db',
+        "PORT": os.environ.get('POSTGRES_PORT'),
+    },
+    'test': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
