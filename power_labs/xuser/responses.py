8a820fc5b4b47dc20a601ab7a3ccbb0ff251883f
saviganga <akatugbasaviour@gmail.com>
2024-07-28 09:25:29 +0100

    feat: test project end to end

--- power_labs/xuser/responses.py
+++ power_labs/xuser/responses.py
@@ -24,7 +24,7 @@ class UserResponses:
         if data == None and message == None:
             return {
                 "status": "SUCCESS",
-                "message": "Success",
+                "message": "Success is ievitable",
             }
         else:
             if data:
